15c0224533b235a5b070c526ffaace9a0596811d
fullbright <full3right@gmail.com>
2020-08-08 20:18:04 +0200

    Fix path to service worker

--- js/pwa-register.js
+++ js/pwa-register.js
@@ -4,7 +4,7 @@ if ("serviceWorker" in navigator) {
   } else {
     // Register the service worker
     navigator.serviceWorker
-      .register("{{ site.baseurl }}/serviceworker.js", {
+      .register("{{ site.baseurl }}/js/serviceworker.js", {
         scope: "./"
       })
       .then(function (reg) {
